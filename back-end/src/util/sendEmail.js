0edc05d98782d10333966b0f9b68f89d0050a637
ygaurav722 <gauravyadav72201@gmail.com>
2022-10-04 16:19:20 +0530

    qdf

--- back-end/src/util/sendEmail.js
+++ back-end/src/util/sendEmail.js
@@ -1,12 +1,8 @@
 const mailgun = require("mailgun-js");
 
-const API_KEY = 'bb789f9ff236e4e014665bb3af71e3b5-381f2624-e6931535';
-const DOMAIN = 'sandbox8f7423abf7174be382c39ff561fa69d8.mailgun.org';
-
-
 export const sendEmail = ({ to, from, subject, text}) => {
 
-    const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
+    const mg = mailgun({apiKey: process.env.API_KEY, domain: process.env.DOMAIN});
     const data = { to, from, subject, text };
     mg.messages().send(data ,(error, body) =>{
         if(error) return console.log(err);
